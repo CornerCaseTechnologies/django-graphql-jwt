5138468461fb589ea260581bc3b1135aa3457ec4
Darius Varnelis <darius.varnelis@cornercasetech.com>
2022-10-24 10:26:28 +0300

    Use double quates for consistency

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -100,4 +100,4 @@ class CSRFRotationTests(TestCase):
             lambda cls, root, info, *args, **kwargs: None,
         )(self, None, info_mock)
 
-        self.assertTrue(info_mock.context.META['CSRF_COOKIE_NEEDS_UPDATE'])
+        self.assertTrue(info_mock.context.META["CSRF_COOKIE_NEEDS_UPDATE"])
